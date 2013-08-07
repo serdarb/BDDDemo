67d385e805be0b16f1d1e5d5767fce90e3f2b875
Serdar Büyüktemiz <hserdarb@gmail.com>
2013-08-07 11:33:35 +0300

    first demo done

---
+++ Specs/ScoringSteps.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace Specs
+{
+    using NUnit.Framework;
+
+    using SuperBowlingScorer;
+
+    [Binding]
+    public class ScoringSteps {
+        private SuperScorer _scorer;
+        [Given(@"I am on the first frame")]
+        public void GivenIAmOnTheFirstFrame()
+        {
+            _scorer = new SuperScorer();
+            Assert.AreEqual(1, _scorer.Frame);
+        }
+
+        [When(@"I bowl a strike")]
+        public void WhenIBowlAStrike()
+        {
+            _scorer.ScoreFirtsBall(10);
+        }
+
+        [Then(@"I should see an ""(.*)"" and a message that says ""(.*)""")]
+        public void ThenIShouldSeeAnAndAMessageThatSays(string p0, string p1)
+        {
+            Assert.AreEqual("Good job!",_scorer.BowlerMessage);
+            Assert.AreEqual("X", _scorer.Score);
+        }
+    }
+}
---
+++ SuperBowlingScorer/SuperScorer.cs
@@ -0,0 +1,29 @@
+﻿namespace SuperBowlingScorer
+{
+    public class SuperScorer
+    {
+        public int Frame { get; set; }
+
+        public string Score { get; set; }
+        public string BowlerMessage { get; set; }
+
+        public SuperScorer()
+        {
+            Frame = 1;
+        }
+
+        private bool IsStrike(bool isFirstBall, int pinsKnockedDown)
+        {
+            return isFirstBall && pinsKnockedDown == 10;
+        }
+
+        public void ScoreFirtsBall(int pinsKnockedDown)
+        {
+            if (IsStrike(true, pinsKnockedDown))
+            {
+                Score = "X";
+                BowlerMessage = "Good job!";
+            }
+        }
+    }
+}
